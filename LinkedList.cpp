f21d9bf879a5ddab32bc1b607869c11680c9ffcc
Dhaval572 <dp9718776@gmail.com>
2024-08-16 18:23:40 +0530

    creating linked list

---
+++ LinkedList.cpp
@@ -0,0 +1,45 @@
+// Creating linked list in c++
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+
+	// Traversing of linked list
+	static void traversalList(Node *ptr)
+	{
+		while (ptr != NULL)
+		{
+			cout << "Element: " << ptr->data << endl;
+			ptr = ptr->next;
+		}
+	}
+};
+
+int main()
+{
+	// Memory allocation for creating linked list
+	Node *head = new Node;
+	Node *second = new Node;
+	Node *third = new Node;
+	Node *fourth = new Node;
+
+	head->data = 1;
+	head->next = second; // head connected to second
+
+	second->data = 2;
+	second->next = third; // head connected to third
+
+	third->data = 3;
+	third->next = fourth; // head connected to fourth
+
+	fourth->data = 4;
+	fourth->next = NULL; // head connected to null
+
+	Node ::traversalList(head); // Static function can call using class name
+
+	return 0;
+}
