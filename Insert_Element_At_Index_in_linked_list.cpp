5e4d305b187215382f51ae955388c62e9e4caa7b
Dhaval572 <dp9718776@gmail.com>
2024-08-22 12:09:56 +0530

    Create Insert_Element_At_Index_in_linked_list.cpp

---
+++ Insert_Element_At_Index_in_linked_list.cpp
@@ -0,0 +1,74 @@
+// Insertion in linked list at index
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+
+	// Traversing of linked list
+	static void traversalList(Node *ptr)
+	{
+		cout << "Element of linked list: ";
+		while (ptr != NULL)
+		{
+			cout << ptr->data << ' '; // printing elements of ptr
+			ptr = ptr->next;		  // ptr updating to print next element
+		}
+	}
+
+	// Insertion in linked list at index
+	static Node *insertAtIndex(Node *head, int index, int data)
+	{
+		Node *newNode = new Node;
+		Node *current = head;				
+
+		// Special case: Inserting at index 0 (beginning of the list)
+		if (index == 0)
+		{
+			newNode->next = head; 	// newNode connected to head
+			newNode->data = data;	
+			return newNode;
+		}
+
+		for (int i = 0; i != (index - 1); i++)   
+		{
+			current = current->next;			// current Node updates 
+		}
+		newNode->data = data;
+		newNode->next = current->next;			// newNode connected to current
+		current->next = newNode;				// current connected to newNode
+		return head;
+	}
+};
+
+int main()
+{
+	// Memory allocation for creating linked list
+	Node *head = new Node;
+	Node *second = new Node;
+	Node *third = new Node;
+	Node *fourth = new Node;
+
+	head->data = 1;
+	head->next = second; // head connected to second
+
+	second->data = 2;
+	second->next = third; // second connected to third
+
+	third->data = 3;
+	third->next = fourth; // third connected to fourth
+
+	fourth->data = 4;
+	fourth->next = NULL; // fourth connected to null
+
+	cout << "Before insertion: " << endl;
+	Node::traversalList(head); // Static function can call using class name
+
+	cout << "\nAfter insertion: " << endl;
+	head = Node::insertAtIndex(head, 3, 45); // head updated
+	Node::traversalList(head);				 // Static function can call using class name
+	return 0;
+}
