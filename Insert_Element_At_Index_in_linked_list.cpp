ddf6284783b8f1f945cd3f3280ffdd3ac92db2e9
Dhaval572 <dp9718776@gmail.com>
2024-08-24 12:35:28 +0530

    Update Insert_Element_At_Index_in_linked_list.cpp

--- Insert_Element_At_Index_in_linked_list.cpp
+++ Insert_Element_At_Index_in_linked_list.cpp
@@ -79,5 +79,5 @@ int main()
 	Note : index and Node both are different
 	Index is index of Linked list. 
 	Node is element of linked list
-	And also insert after node and insert at index both are different.
+	And also 'insert after node' and 'insert at index' both are different.
 */ 
