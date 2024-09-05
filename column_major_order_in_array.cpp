d9780fcd95decf91b660f187435b9209298baff6
Dhaval572 <dp9718776@gmail.com>
2024-09-05 13:39:41 +0530

    Create column_major_order_in_array.cpp

---
+++ column_major_order_in_array.cpp
@@ -0,0 +1,26 @@
+// Column major order in C++
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int rows = 3;
+    const int cols = 4;
+
+    // Initialize a 2D array
+    int array[rows][cols] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    // Print the array in column-major order
+    cout << "Array in Column Major Order:" << endl;
+    for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
