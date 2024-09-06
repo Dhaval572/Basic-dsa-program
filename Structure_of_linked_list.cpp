ff78b503e92cf52dc614c06a55980fc009fba86f
Dhaval572 <dp9718776@gmail.com>
2024-09-06 12:22:32 +0530

    Create Structure_of_linked_list.cpp
    Copy the entire code into VS Code to view the complete diagram perfectly.

---
+++ Structure_of_linked_list.cpp
@@ -0,0 +1,33 @@
+// Note : copy this all to vs code to look diagram perfect
+// Diagram of linked list:
+
+/*
+    +--------+     +--------+     +--------+     +--------+
+    | Data: 1| ->  | Data: 2| ->  | Data: 3| ->  | Data: 4| -> nullptr
+    | Next:->|     | Next:->|     | Next:->|     | Next: n|
+    +--------+     +--------+     +--------+     +--------+
+*/
+
+/*
+
+    Upper side is a diagram of linked list in which all elements are connected to his next element
+
+    So this structure is looked link chain, so there for it is called linked list means that all elements is linked with other elements.
+
+    In linked list all elements are called node.
+
+    In linked list first node is known as head node.
+
+    In linked list always remember that last Node always connects to the null.
+
+    So here is more one simple diagram of node:
+
+    [Head Node] -> [next Node] -> [next Node] -> [Next Node] -> null
+
+    More simplification:
+
+    [Node 1] -> [Node 2] -> [Node 3] -> [Node 4] -> null
+
+    I hope you understand the structure of a linked list.
+
+*/
