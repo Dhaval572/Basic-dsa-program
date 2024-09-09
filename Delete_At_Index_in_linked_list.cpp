db4afa9ba53631afe1178ede550757b632c0027c
Dhaval572 <dp9718776@gmail.com>
2024-09-09 22:07:11 +0530

    Update Delete_At_Index_in_linked_list.cpp

--- Delete_At_Index_in_linked_list.cpp
+++ Delete_At_Index_in_linked_list.cpp
@@ -5,64 +5,74 @@ using namespace std;
 class Node
 {
 public:
-    int data;
-    Node *next;
+	int data;
+	Node *next;
 
-    // Traversing of linked list
-    static void traversalList(Node *currentNode)
-    {
-        cout << "Element of linked list: ";
-        while (currentNode != nullptr)
-        {
-            cout << currentNode->data << ' '; // printing elements of linked list
-            currentNode = currentNode->next;
-        }
-    }
+	// Traversing of linked list
+	static void traversalList(Node *currentNode)
+	{
+		cout << "Element of linked list: ";
+		while (currentNode != nullptr)
+		{
+			cout << currentNode->data << ' '; // printing elements of linked list
+			currentNode = currentNode->next;
+		}
+	}
 
-    // Deleting the Node at a given index from the linked list
-    static Node *InsertAtIndex(Node *head, int index)
-    {
-        Node *p = head;
-        Node *q = head->next;
-        
-        for (int i = 0; i < (index - 1); i++)
-        {
-            p = p->next;
-            q = q->next;
-        }
-        
-        p->next = q->next;
-        delete q;
-        return head;
-    }
+	// Deleting the Node at a given index from the linked list
+	static Node *InsertAtIndex(Node *head, int index)
+	{
+
+		// Special case: deletion at index 0
+		if (index == 0)
+		{
+			Node *nodeToDelete = head;
+			head = head->next;
+			delete nodeToDelete;  // Dellocate memory of deleted node
+			return head;
+		}
+
+		Node *previousNode = head;
+		Node *nodeToDelete = head->next;
+
+		for (int i = 0; i < (index - 1); i++)
+		{
+			previousNode = previousNode->next;
+			nodeToDelete = nodeToDelete->next; 
+		}
+
+		previousNode->next = nodeToDelete->next;
+		delete nodeToDelete; // Dellocate memory of deleted node
+		return head;
+	}
 };
 
 int main()
 {
-    // Memory allocation for creating linked list
-    Node *head = new Node;
-    Node *second = new Node;
-    Node *third = new Node;
-    Node *fourth = new Node;
+	// Memory allocation for creating linked list
+	Node *head = new Node;
+	Node *second = new Node;
+	Node *third = new Node;
+	Node *fourth = new Node;
 
-    head->data = 1;
-    head->next = second; // head connected to second
+	head->data = 1;
+	head->next = second; // head connected to second
 
-    second->data = 2;
-    second->next = third; // second connected to third
+	second->data = 2;
+	second->next = third; // second connected to third
 
-    third->data = 3;
-    third->next = fourth; // third connected to fourth
+	third->data = 3;
+	third->next = fourth; // third connected to fourth
 
-    fourth->data = 4;
-    fourth->next = nullptr; // fourth connected to null
+	fourth->data = 4;
+	fourth->next = nullptr; // fourth connected to null
 
-    cout << "Before insertion: " << endl;
-    Node ::traversalList(head); // Static function can call using class name
+	cout << "Before insertion: " << endl;
+	Node::traversalList(head); // Static function can call using class name
 
-    head = Node::InsertAtIndex(head,2);
-    cout << "\nAfter insertion: " << endl;
-    Node::traversalList(head);
+	head = Node::InsertAtIndex(head, 2);
+	cout << "\nAfter insertion: " << endl;
+	Node::traversalList(head);
 
-    return 0;
+	return 0;
 }
