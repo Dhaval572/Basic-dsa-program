ef86a83a79df3a876bee4fd35586fd73cc148c9e
Dhaval572 <dp9718776@gmail.com>
2024-09-21 11:16:34 +0530

    Create About_Tree_data_structure.cpp

---
+++ About_Tree_data_structure.cpp
@@ -0,0 +1,40 @@
+// About tree data structure
+
+/*
+
+=>	A tree data structure is a way to organize data in a hierarchy, like a family tree, that makes it easy to find, add, or remove items.
+
+=>	Tree data structures are non-linear data structures.
+
+Structure of tree is: 
+
+	    A         <-- Root Node
+       / \
+      B   C       <-- Child Nodes
+     / \   \
+    D   E   F     <-- Leaf Nodes
+
+	In upper side, you can see the basic structure of tree.
+
+	Root Node: 
+
+		This node is a top most node of the tree.
+
+	Child node: 
+
+		This node is connected by another node.
+
+	Leaf node: 
+
+		Node with no children means you can say that this is a last node of tree.
+		
+		( Note: leaf node is also known as external node )
+
+	Internal node: 
+
+		This node contains atleast one child node. So this is a apposite of External node.
+
+	So, This is a basic strcuture of tree data structure.
+
+	I hope you understande!!
+*/
