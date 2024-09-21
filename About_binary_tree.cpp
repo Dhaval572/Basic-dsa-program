ceabcb152951ab3fc5ab95f2aa7e0d7112bdec2f
Dhaval572 <dp9718776@gmail.com>
2024-09-21 17:49:50 +0530

    Create About_binary_tree.cpp

---
+++ About_binary_tree.cpp
@@ -0,0 +1,71 @@
+// About binary tree
+
+/*( Note: copy all the text into vs code to look perfect diagram otherwise it will become difficult to understand )*/
+
+/*
+
+=>	A binary tree is a non-linear data structure where each node has at most two children.
+
+=>	It means in binary tree, you must have maximum 2 child nodes and minimum 0.
+
+Diagram of Binary tree:
+
+	  A
+	 / \
+	B   C
+   / \   \
+  D   E   F
+
+Components Explain:
+
+1. Node:
+	
+	Each letter (A, B, C, D, E, F) represents a node containing data.
+
+2. Root: 
+	
+	The topmost node is A, which serves as the starting point for the tree.
+
+3. Edges:
+	
+	The lines connecting the nodes represent edges. 
+	
+	For example, the edge connects A to B and A to C.
+
+4. Children:
+
+	B and C are children of A.
+	D and E are children of B.
+	F is a child of C.
+
+5. Parent:
+
+	A is the parent of B and C.
+	B is the parent of D and E.
+	C is the parent of F.
+
+6. Leaf Nodes:
+	
+	Nodes with no children are called leaf nodes.
+
+	In this tree, D, E, and F are leaf nodes.
+
+7. Height: 
+	
+	The height of the tree is the longest path from the root to a leaf. 
+	
+	Here, the height is 2 (from A to D, E, or F).
+
+8. Depth: 
+
+	The depth of a node is the number of edges from the root to that node.
+
+	For example:-
+
+		Depth of A = 0
+		Depth of B = 1
+		Depth of C = 1
+		Depth of D = 2
+		Depth of E = 2
+		Depth of F = 2
+*/
