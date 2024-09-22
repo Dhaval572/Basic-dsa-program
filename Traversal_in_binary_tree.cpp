172964ba1dd91aed1a13943837aa2553dc50339a
Dhaval572 <dp9718776@gmail.com>
2024-09-22 11:48:05 +0530

    Create Traversal_in_binary_tree.cpp

---
+++ Traversal_in_binary_tree.cpp
@@ -0,0 +1,36 @@
+// Travertion explaination in binary tree
+
+/*
+
+So, in this we will see that how to perform traversal operation in binary tree So get start.
+
+Travertion:
+
+	Travertion is a proccess in which we access all element of data structure.
+
+Explaination of traversal in binary search:
+
+	In binary tree, there are three ways to travers tree
+
+	1. Pre-order traversal:
+
+		Pre-order traversal is a way to visit nodes in a binary tree where you check the current node first, then the left child, and finally the right child. 
+		
+		It follows this order: Root → Left → Right.
+
+	2. Post-order traversal: 
+
+		Post-order traversal is a way to visit nodes in a binary tree where you check the left child first, then the right child, and finally the current node. 
+		
+		It follows this order: Left → Right → Root.
+
+	3. In-order traversal: 
+
+		In-order traversal is a way to visit nodes in a binary tree where you check the left child first, then the current node, and finally the right child. 
+		
+		It follows this order: Left → Root → Right.
+
+	So, that all are types of traversal in binary tree
+
+	I hope you understand!!
+*/
