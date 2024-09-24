ef732e98136cc09a3c4ca6f7aac47d28bc0fe999
Dhaval572 <dp9718776@gmail.com>
2024-09-24 19:00:21 +0530

    Create About_binary_search_tree.cpp

---
+++ About_binary_search_tree.cpp
@@ -0,0 +1,38 @@
+// Binary search tree 
+
+/*
+( Note: copy all text into vs code to view properly )
+
+A binary search tree (BST) is a type of binary tree that has the following properties:
+
+	Properties:
+
+		1). All nodes of left sub-tree are less than his root node.
+
+		2). All nodes of right sub-tree are greterthan than his root node.
+
+		3). Left and Right sub-trees are also binary tree.
+			it means sub-trees have maximum 2 child nodes.
+		
+		4). In this type of trees, there are not allowed duplicate nodes.
+
+		5). In-order traversal of a binary search tree gives an ascending sorted array.
+
+	So, here is one example of binary search tree:
+
+	    10
+       /  \
+      5    15
+     / \   / \
+    3   7 12  17
+
+	So, you can check that all properties are working or not.
+
+	SO, let's travers this tree in In-order.
+
+	In-order traversal: 3, 5, 7, 10, 12, 15, 17
+
+	So, you can see that These all values are sorted.( No. 5 properties )
+
+	So, i hope you understand everything!
+*/
