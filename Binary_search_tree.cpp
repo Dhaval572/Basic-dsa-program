173a706abad72694d01428a137b0d22891ec76d6
Dhaval572 <dp9718776@gmail.com>
2024-09-24 19:45:39 +0530

    Update Binary_search_tree.cpp

--- Binary_search_tree.cpp
+++ Binary_search_tree.cpp
@@ -18,52 +18,45 @@ public:
         newNode->right = nullptr;  // Setting right child node to NULL
         return newNode;            // Returning the created Node;
     }
+
+	static void *inOrderTraversal(Node *root)
+	{
+		if (root != nullptr)
+		{
+			inOrderTraversal(root->left);
+			cout << root->data << ' ';
+			inOrderTraversal(root->right);
+		}
+	}
 };
-
-// Function to insert a new node in the BST
-Node* insert(Node* root, int data) {
-    // If the tree is empty, return a new node
-    if (root == nullptr) {
-        return Node::createNode(data);
-    }
-
-    // Otherwise, recur down the tree
-    if (data < root->data) {
-        root->left = insert(root->left, data); // Insert in left subtree
-    } else {
-        root->right = insert(root->right, data); // Insert in right subtree
-    }
-
-    // Return the (unchanged) node pointer
-    return root;
-}
-
-// Function to perform inorder traversal of the BST
-void inorderTraversal(Node* root) {
-    if (root != nullptr) {
-        inorderTraversal(root->left);   // Visit left subtree
-        cout << root->data << " ";      // Visit node
-        inorderTraversal(root->right);  // Visit right subtree
-    }
-}
 
 int main()
 {
-    Node *rootNode = nullptr; // Start with an empty tree
+    Node *rootNode = Node::createNode(27);      // Creating a rootNode
+    Node *parentNode1 = Node::createNode(25);   // Creating a parentNode1
+    Node *parentNode2 = Node::createNode(30);   // Creating a parentNode2
+    Node *childNode1 = Node::createNode(28);    // Creating a childNode1
+    Node *childNode2 = Node::createNode(35);    // Creating a childNode2
+    Node *leafNode1 = Node::createNode(26);      // Creating a leafNode1
+    Node *leafNode2 = Node::createNode(32);      // Creating a leafNode2
 
-    // Creating nodes with appropriate values for a BST
-    rootNode = insert(rootNode, 27);      // Creating a rootNode
-    insert(rootNode, 15);                  // Creating a node with value 15
-    insert(rootNode, 35);                  // Creating a node with value 35
-    insert(rootNode, 10);                  // Creating a node with value 10
-    insert(rootNode, 20);                  // Creating a node with value 20
-    insert(rootNode, 30);                  // Creating a node with value 30
-    insert(rootNode, 40);                  // Creating a node with value 40
+    // Linking a rootNode with left and right childNodes
+    rootNode->left = parentNode1;
+    rootNode->right = parentNode2;
 
-    // Inorder traversal of the BST
-    cout << "Inorder Traversal of the BST: ";
-    inorderTraversal(rootNode);
-    cout << endl;
+    // Linking a parentNodes to his child nodes
+    parentNode1->right = leafNode1; 
+    parentNode2->left = childNode1;  
+    parentNode2->right = childNode2; 
+
+    // Linking a childNodes to leafNodes
+    childNode2->left = leafNode2; 
+    childNode1->left = nullptr;  
+    childNode1->right = nullptr;  
+    childNode2->right = nullptr; 
+
+	cout << "Inorder traversal of binary search tree: ";
+	Node::inOrderTraversal(rootNode); 
 
     return 0;
 }
@@ -74,4 +67,5 @@ int main()
 	So, you all know that in binary search tree (BST), Inorder traversal is like as an ascending sorted array.
 
 	If you don't know that what is inorder traversal, than explore my github link "https://github.com/Dhaval572/Basic-dsa-program" and search this: 'In_order_traversal.cpp'
+
 */
