54a8070b51f3cfb10a13ff42ae043cd0f2880512
Dhaval572 <dp9718776@gmail.com>
2024-09-29 12:14:48 +0530

    Update Insertion_sort.cpp

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,3 +1,4 @@
+// Insertion sort in c++
 #include <iostream>
 using namespace std;
 
@@ -49,3 +50,44 @@ int main()
     display(size, arr);
     return 0;
 }
+/*
+Explaination: 
+
+1. Initial Setup:
+
+    Start with an array and consider the first element as a sorted portion. 
+
+2. Selecting the Key:  
+   
+    Take the next element (the "key") from the unsorted portion of the array. 
+
+3. Comparing and Shifting:  
+    
+    Compare the key with elements in the sorted portion:
+   
+    ->  If the key is smaller than the compared element, shift the compared element 
+        one position to the right to make space for the key.
+
+4. Inserting the Key:
+   
+    Once the correct position is found (when you find an element not greater than the key), insert the key into that position.
+
+5. Repeat:  
+   
+    Move to the next element in the unsorted portion and repeat the process:
+   
+    ->  Continue selecting keys and inserting them into their correct positions in 
+        the sorted portion.
+
+6. Continue: 
+    
+    Continue this process until all elements are sorted.
+
+In Short:  
+
+    In insertion sort, you build a sorted portion of the array by repeatedly taking 
+    the next unsorted element and inserting it into the correct position within the 
+    sorted portion.
+
+    I hope you understand!
+*/
