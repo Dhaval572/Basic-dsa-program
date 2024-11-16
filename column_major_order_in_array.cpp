8d0458b5b49fd2827dc923278d960221bf11a332
Dhaval572 <dp9718776@gmail.com>
2024-11-16 16:21:28 +0530

    Format Documentation

--- column_major_order_in_array.cpp
+++ column_major_order_in_array.cpp
@@ -2,25 +2,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    const int rows = 3;
-    const int cols = 4;
+int main()
+{
+	const int rows = 3;
+	const int cols = 4;
 
-    // Initialize a 2D array
-    int array[rows][cols] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12}
-    };
+	// Initialize a 2D array
+	int array[rows][cols] = {
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12}};
 
-    // Print the array in column-major order
-    cout << "Array in Column Major Order:" << endl;
-    for (int j = 0; j < cols; j++) {
-        for (int i = 0; i < rows; i++) {
-            cout << array[i][j] << " ";
-        }
-        cout << endl;
-    }
+	// Print the array in column-major order
+	cout << "Array in Column Major Order:" << endl;
+	for (int j = 0; j < cols; j++)
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			cout << array[i][j] << " ";
+		}
+		cout << endl;
+	}
 
-    return 0;
+	return 0;
 }
